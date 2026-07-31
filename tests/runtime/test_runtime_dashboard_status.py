5c35106e32d2c9bea4f7458a826a56feaf9eb0c0
HANNINGTON OTIENO <onyi.dulo@gmail.com>
2026-07-30 21:24:05 -0500

    test(runtime): reject non-numeric normalized confidence values

--- tests/runtime/test_runtime_dashboard_status.py
+++ tests/runtime/test_runtime_dashboard_status.py
@@ -9302,3 +9302,95 @@ def test_acceptance_confidence_rejects_boolean_values(
         make_status(
             acceptance_confidence=boolean_value,
         )
+
+@pytest.mark.parametrize(
+    "field_name",
+    (
+        "institutional_bias_confidence",
+        "institutional_bias_strength",
+        "institutional_bias_bullish_score",
+        "institutional_bias_bearish_score",
+        "market_phase_confidence",
+        "market_phase_strength",
+        "confluence_score",
+        "acceptance_confidence",
+        "trend_confidence",
+        "structure_confidence",
+        "liquidity_confidence",
+        "order_block_confidence",
+        "auction_confidence",
+        "pressure_confidence",
+        "participation_confidence",
+        "value_confidence",
+        "analyst_average_confidence",
+        "analyst_enabled_average_confidence",
+        "analyst_confidence_coverage_percentage",
+        "analyst_enabled_confidence_coverage_percentage",
+        "analyst_coverage_percentage",
+        "analyst_operational_percentage",
+    ),
+)
+@pytest.mark.parametrize(
+    "invalid_value",
+    (
+        "50",
+        "",
+        [],
+        {},
+        object(),
+    ),
+)
+def test_normalized_confidence_fields_reject_non_numeric_values(
+    field_name: str,
+    invalid_value: object,
+) -> None:
+    with pytest.raises(
+        TypeError,
+        match=rf"{field_name} must be a number or None",
+    ):
+        make_status(
+            **{
+                field_name: invalid_value,
+            },
+        )
+
+@pytest.mark.parametrize(
+    "field_name",
+    (
+        "institutional_bias_confidence",
+        "institutional_bias_strength",
+        "institutional_bias_bullish_score",
+        "institutional_bias_bearish_score",
+        "market_phase_confidence",
+        "market_phase_strength",
+        "confluence_score",
+        "acceptance_confidence",
+        "trend_confidence",
+        "structure_confidence",
+        "liquidity_confidence",
+        "order_block_confidence",
+        "auction_confidence",
+        "pressure_confidence",
+        "participation_confidence",
+        "value_confidence",
+        "analyst_average_confidence",
+        "analyst_enabled_average_confidence",
+        "analyst_confidence_coverage_percentage",
+        "analyst_enabled_confidence_coverage_percentage",
+        "analyst_coverage_percentage",
+        "analyst_operational_percentage",
+    ),
+)
+def test_normalized_confidence_fields_allow_none(
+    field_name: str,
+) -> None:
+    status = make_status(
+        **{
+            field_name: None,
+        },
+    )
+
+    assert getattr(
+        status,
+        field_name,
+    ) is None
